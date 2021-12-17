2065d6dc9ec3a3b4f0d4751721986bb5462df15a
Damon <damondias@gmail.com>
2021-12-17 01:13:42 -0400

    configs iniciais do projeto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+// import { Container } from './styles';
+
+function App() {
+  return (
+    <p>Em construção </p>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/reset.css";
+import "./styles/index.css";
+
+const root = document.querySelector(".root");
+ReactDOM.render(<App/>, root);
