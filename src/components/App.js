356556f74ee0b9470d7a8d33f876a2dee3edf460
Damon <damondias@gmail.com>
2021-12-20 10:33:04 -0400

    Instalando bibliotecas, componetizando e gerando rotas

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
-
-// import { Container } from './styles';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './Header';
+import Catalogo from "./Catalogo";
+import Filme from "./Filme";
+import Sessao from "./Sessao";
 
 function App() {
+  const [idFilme, setIdFilme] = React.useState(null);
   return (
-    <p>Em construção </p>
+    <BrowserRouter>      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Catalogo setIdFilme={setIdFilme}/>}></Route>
+        <Route path="/filme/:idFilme" element={<Filme />}></Route>
+        <Route path="/sessao/:idSessao" element={<Sessao />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
